b25ad646db8af5bbb76c38acaddee9e0329c10bb
Volodymyr Korolchuk <gitpub10@gmail.com>
2024-04-07 19:52:41 +0300

    add: navbar search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ import {
 } from "./ui/dropdown-menu";
 import { Link, useLocation } from "react-router-dom";
 import { publicRoutes } from "./../../routes";
+import { Input } from "./ui/input";
 
 const Navbar: React.FC = ({}) => {
   const location = useLocation();
@@ -32,20 +33,24 @@ const Navbar: React.FC = ({}) => {
 
   const userProfile = (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="font-chakra">
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarFallback>JD</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="dark w-56" align="end" forceMount>
+      <DropdownMenuContent
+        className="font-chakra dark w-56"
+        align="end"
+        forceMount
+      >
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">John Doe</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              johndoe217@gmail.com
             </p>
           </div>
         </DropdownMenuLabel>
@@ -56,14 +61,9 @@ const Navbar: React.FC = ({}) => {
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
             Settings
             <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
           </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
@@ -75,12 +75,16 @@ const Navbar: React.FC = ({}) => {
   );
 
   return (
-    <header className="flex items-center justify-between w-screen bg-dark p-4 px-5 h-16 bg-neutral-950">
-      <h2 className="text-white font-normal font-chakra">RentIO</h2>
+    <header className="flex items-center font-chakra justify-between w-screen bg-dark p-4 px-5 h-16 bg-neutral-950">
+      <h2 className="text-neutral-400 font-normal">RentIO</h2>
 
-      <nav className="flex items-center justify-between gap-5">
-        {isPublicRoute ? publicLinkElements : userProfile}
-      </nav>
+      <div className="flex items-center justify-between gap-5">
+        <Input className="dark text-white" placeholder="Search..." />
+
+        <nav className="flex items-center justify-between">
+          {isPublicRoute ? publicLinkElements : userProfile}
+        </nav>
+      </div>
     </header>
   );
 };
